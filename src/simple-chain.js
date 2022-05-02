2fd5b671cf8ef48ed684f8f559159e45a9a4ec6d
‘loitez’ <loitez@mail.ru>
2022-05-03 00:55:13 +0300

    commit

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    if (!value) {
+      this.chain.push('(  )')
+      return this
+    } else {
+      this.chain.push(`( ${value} )`)
+      return this
+    }
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (typeof position !== 'number' || position % 2 !== 0 || position >= this.length || position <= 0) {
+      this.chain = []
+      this.length = 0;
+      throw new Error ('You can\'t remove incorrect link!')
+    } else {
+      this.chain.splice(position - 1, 1)
+      return this
+    }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse()
+    return this
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain = this.chain.join('~~')
+    return this.chain
   }
 };
 
