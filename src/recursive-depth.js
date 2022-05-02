38725cb0cedc1d2ca2adf75e8d91a5045e6edcbd
‘loitez’ <loitez@mail.ru>
2022-05-03 01:28:30 +0300

    commit

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -18,7 +18,11 @@ let depth = []*/
 
 class DepthCalculator {
   calculateDepth(arr) {
-
+    if(arr.filter(i => i.constructor.name === "Array").length != 0){
+      return 1 + this.calculateDepth([].concat(...arr.filter(i => i.constructor.name === "Array")));
+    } else {
+      return 1;
+    }
   }
 }
 
