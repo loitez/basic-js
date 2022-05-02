46271b0d0de5acac0b7e95d7f35919c4fb91a0d6
‘loitez’ <loitez@mail.ru>
2022-05-02 20:37:19 +0300

    commit

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,11 +17,30 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
 
+dateSample('1')
+function dateSample(sampleActivity) {
+  let MODERN_ACTIVITY = 15;
+  let HALF_LIFE_PERIOD = 5730;
+  let number = 0.693;
+  let k = number / HALF_LIFE_PERIOD
+  if (typeof sampleActivity != 'string') {
+    return false
+  } else if (!Number(sampleActivity)) {
+    return false
+  } else if (sampleActivity <=0 || sampleActivity > 15) {
+    return false
+  }
+  if (/[0-9]/.test(sampleActivity)) {
+    let T = (Math.log(MODERN_ACTIVITY/+sampleActivity))/k;
+    T = Math.ceil(T)
+    console.log(T)
+    return T
+  } else {
+    return false
+  }
+  return false
+}
 module.exports = {
   dateSample
 };
